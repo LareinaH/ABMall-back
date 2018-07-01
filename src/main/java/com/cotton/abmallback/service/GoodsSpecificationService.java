320221bbbd821b16130de296b518fb551903f333
lquterqtd <378203086@qq.com>
2018-07-01 16:19:19 +0800

    add

--- src/main/java/com/cotton/abmallback/service/GoodsSpecificationService.java
+++ src/main/java/com/cotton/abmallback/service/GoodsSpecificationService.java
@@ -2,6 +2,9 @@ package com.cotton.abmallback.service;
 
 import com.cotton.base.service.BaseService;
 import com.cotton.abmallback.model.GoodsSpecification;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * GoodsSpecification
@@ -11,4 +14,6 @@ import com.cotton.abmallback.model.GoodsSpecification;
  * @date 2018/5/22
  */
 public interface GoodsSpecificationService extends BaseService<GoodsSpecification> {
+    @Select("select distinct(goods_specification_name) from goods_specification order by goods_specification_name")
+    public List<String> getSpecUnitList();
 }
