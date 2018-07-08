98d617fd5d2925436641029637f818b73b0ff3bd
lu.han <lu.han@tongdun.cn>
2018-07-08 22:49:05 +0800

    修正分组的逻辑

--- src/main/java/com/cotton/abmallback/web/controller/admin/GoodsGroupManagerController.java
+++ src/main/java/com/cotton/abmallback/web/controller/admin/GoodsGroupManagerController.java
@@ -1,17 +1,22 @@
 package com.cotton.abmallback.web.controller.admin;
 
+import com.cotton.abmallback.model.Goods;
 import com.cotton.abmallback.model.GoodsGroup;
+import com.cotton.abmallback.model.vo.admin.GoodsGroupVO;
 import com.cotton.abmallback.service.GoodsGroupService;
+import com.cotton.abmallback.service.GoodsService;
 import com.cotton.base.common.RestResponse;
 import com.cotton.base.controller.BaseController;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,9 +36,12 @@ public class GoodsGroupManagerController extends BaseController {
 
     private GoodsGroupService goodsGroupService;
 
+    private GoodsService goodsService;
+
     @Autowired
-    public GoodsGroupManagerController(GoodsGroupService goodsGroupService) {
+    public GoodsGroupManagerController(GoodsGroupService goodsGroupService, GoodsService goodsService) {
         this.goodsGroupService = goodsGroupService;
+        this.goodsService = goodsService;
     }
 
     @ResponseBody
@@ -100,9 +108,9 @@ public class GoodsGroupManagerController extends BaseController {
 
     @ResponseBody
     @RequestMapping(value = "/queryPageList", method = {RequestMethod.POST})
-    public RestResponse<PageInfo<GoodsGroup>> queryPageList(@RequestParam(defaultValue = "1") int pageNum,
-                                                            @RequestParam(defaultValue = "4") int pageSize,
-                                                            @RequestBody()Map<String,Object> conditions) {
+    public RestResponse<PageInfo<GoodsGroupVO>> queryPageList(@RequestParam(defaultValue = "1") int pageNum,
+                                                              @RequestParam(defaultValue = "4") int pageSize,
+                                                              @RequestBody()Map<String,Object> conditions) {
 
         Example example = new Example(GoodsGroup.class);
         example.setOrderByClause("gmt_create desc");
@@ -124,13 +132,44 @@ public class GoodsGroupManagerController extends BaseController {
             logger.error("读取列表失败");
             return RestResponse.getSystemInnerErrorResponse();
         }
-        return RestResponse.getSuccesseResponse(goodsGroupPageInfo);
+
+        PageInfo<GoodsGroupVO> goodsGroupVOPageInfo = new PageInfo<>();
+        BeanUtils.copyProperties(goodsGroupPageInfo,goodsGroupVOPageInfo);
+
+        if(goodsGroupPageInfo.getList() != null  && goodsGroupPageInfo.getList().size() > 0) {
+
+            List<GoodsGroupVO> goodsGroupVOList = new ArrayList<>();
+
+            for (GoodsGroup goodsGroup : goodsGroupPageInfo.getList()) {
+
+                GoodsGroupVO goodsGroupVO = new GoodsGroupVO();
+                BeanUtils.copyProperties(goodsGroup,goodsGroupVO);
+
+                Goods model = new Goods();
+                model.setGroupId(goodsGroup.getId());
+                model.setIsDeleted(false);
+                long count = goodsService.count(model);
+
+                goodsGroupVO.setGoodsCount(count);
+
+                goodsGroupVOList.add(goodsGroupVO);
+
+            }
+        }
+
+
+        return RestResponse.getSuccesseResponse(goodsGroupVOPageInfo);
     }
 
 
     @ResponseBody
     @RequestMapping(value = "/delete", method = {RequestMethod.DELETE})
     public RestResponse<Map<String, Object>> delete(long goodsGroupId) {
+
+        if(goodsGroupId == 0L){
+            return RestResponse.getFailedResponse(500, "不能删除默认分组");
+
+        }
 
         GoodsGroup goodsGroup = goodsGroupService.getById(goodsGroupId);
 
@@ -142,6 +181,19 @@ public class GoodsGroupManagerController extends BaseController {
         goodsGroup.setIsDeleted(true);
 
         if (!goodsGroupService.update(goodsGroup)) {
+
+            Goods model = new Goods();
+            model.setGroupId(goodsGroup.getId());
+            model.setIsDeleted(false);
+
+            List<Goods> goodsList = goodsService.queryList(model);
+
+            if(goodsList != null && goodsList.size() > 0){
+                for(Goods goods : goodsList){
+                    goods.setGroupId(0L);
+                    goodsService.update(goods);
+                }
+            }
             return RestResponse.getFailedResponse(500, "删除数据失败,goodsGroupId为:" + goodsGroupId);
         }
 
