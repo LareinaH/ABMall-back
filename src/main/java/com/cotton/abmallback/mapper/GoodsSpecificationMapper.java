4e348b71a68e6307e9951627dd7a595e9c93a25b
lquterqtd <378203086@qq.com>
2018-07-15 22:48:40 +0800

    bug fix

--- src/main/java/com/cotton/abmallback/mapper/GoodsSpecificationMapper.java
+++ src/main/java/com/cotton/abmallback/mapper/GoodsSpecificationMapper.java
@@ -13,7 +13,7 @@ public interface GoodsSpecificationMapper extends BaseMapper<GoodsSpecification>
     @Select("select distinct(goods_specification_name) from goods_specification order by goods_specification_name")
     List<String> getSpecUnitList();
 
-    @Update("update goods_specification set is_on_sell = #{isOnSale} where goodsId=#{goodsId} and is_deleted = 0")
+    @Update("update goods_specification set is_on_sell = #{isOnSale} where goods_id=#{goodsId} and is_deleted = 0")
     void setGoodsOnSaleStatus(
             @Param(value = "goodsId") long goodsId,
             @Param(value = "isOnSale") int isOnSale
