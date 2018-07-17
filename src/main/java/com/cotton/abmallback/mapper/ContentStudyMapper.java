9d1224aa36ffe801acde6cf092043963233d5872
lu.han <lu.han@tongdun.cn>
2018-07-17 14:32:29 +0800

    增加 教程部分的 代码

---
+++ src/main/java/com/cotton/abmallback/mapper/ContentStudyMapper.java
@@ -0,0 +1,7 @@
+package com.cotton.abmallback.mapper;
+
+import com.cotton.abmallback.model.ContentStudy;
+import com.cotton.base.mapper.BaseMapper;
+
+public interface ContentStudyMapper extends BaseMapper<ContentStudy> {
+}
